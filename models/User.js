4ed92ed56bf2d1c17bfeedf2b166a7ceccd5fc91
alxsaunders <alexsaunders242@gmail.com>
2022-08-01 18:38:55 -0400

    fix bugs

--- models/User.js
+++ models/User.js
@@ -1,36 +1,54 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection.js');
 
-User.hasMany(Post, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-User.hasMany(Comment, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
 
-Post.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'User'
+  }
+);
 
-Post.hasMany(Comment, {
-  foreignKey: 'postId',
-  onDelete: 'CASCADE'
-});
-
-Comment.belongsTo(User, {
-  foreignKey: 'userId',
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: 'postId'
-});
-module.exports = {
-  User,
-  Comment,
-  Post
-};
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,16 +1,7 @@
+
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
-
-User.hasMany(Post, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
 
 Post.belongsTo(User, {
   foreignKey: 'userId',
@@ -24,11 +15,9 @@ Post.hasMany(Comment, {
 
 Comment.belongsTo(User, {
   foreignKey: 'userId',
+  onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(Post, {
-  foreignKey: 'postId'
-});
 module.exports = {
   User,
   Comment,
